d2f4a501cba60db594759537514981a364df17f3
MeetBhatiya23 <meetbhatiya.dds@gmail.com>
2025-08-11 18:14:10 +0530

    fixed-deploy-error

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Typewriter from "typewriter-effect";
 import HeroImg from "../../images/HeroImage.jpg";
 import HeroBgAnimation from "../HeroBgAnimation";
-import { Tilt } from "react-tilt";
+import Tilt from 'react-parallax-tilt';
 import { motion } from "framer-motion";
 import {
     headContainerAnimation,
